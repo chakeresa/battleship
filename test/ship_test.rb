bcb26f5d9479fb8e96e7bf9ccb844cdde44e596e
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-18 17:13:18 -0600

    Add ship initialization of name and length

---
+++ test/ship_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+  def test_it_exists
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, cruiser
+  end
+
+  def test_it_has_a_name
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal "Cruiser", cruiser.name
+  end
+
+  def test_it_has_a_length
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal 3, cruiser.length
+  end
+end
