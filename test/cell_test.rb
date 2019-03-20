c3a99ccfc879256036144d54bd907580573b10b3
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-20 14:22:30 -0600

    Rename place_ship to place and remove coordinate from CellTest

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -3,28 +3,22 @@ require './lib/cell'
 
 class CellTest < Minitest::Test
   def test_it_exists
-    cell = Cell.new("B4")
+    cell = Cell.new
 
     assert_instance_of Cell, cell
   end
 
-  def test_it_has_coord
-    cell = Cell.new("B4")
-
-    assert_equal "B4", cell.coord
-  end
-
   def test_it_starts_with_no_ship
-    cell = Cell.new("B4")
+    cell = Cell.new
 
     assert_nil cell.ship
   end
 
   def test_it_gets_a_ship_when_placed
-    cell = Cell.new("B4")
+    cell = Cell.new
     boat = Ship.new("Boot", 5)
 
-    cell.place_ship(boat)
+    cell.place(boat)
 
     assert_instance_of Ship, cell.ship
     assert_equal boat, cell.ship
@@ -32,17 +26,17 @@ class CellTest < Minitest::Test
   end
 
   def test_it_is_not_fired_upon_initially
-    cell = Cell.new("B4")
+    cell = Cell.new
     boat = Ship.new("Boot", 5)
 
     refute cell.fired_upon?
   end
 
   def test_a_hit
-    cell = Cell.new("B4")
+    cell = Cell.new
     boat = Ship.new("Boot", 5)
 
-    cell.place_ship(boat)
+    cell.place(boat)
     cell.fire_upon
 
     assert cell.fired_upon?
@@ -50,13 +44,13 @@ class CellTest < Minitest::Test
   end
 
   def test_it_initially_renders_dot
-    cell = Cell.new("B4")
+    cell = Cell.new
 
     assert_equal ".", cell.render
   end
 
   def test_it_renders_m_after_miss
-    cell = Cell.new("B4")
+    cell = Cell.new
 
     cell.fire_upon
 
@@ -64,20 +58,20 @@ class CellTest < Minitest::Test
   end
 
   def test_it_renders_h_after_hit
-    cell = Cell.new("B4")
+    cell = Cell.new
     cruiser = Ship.new("Cruiser", 3)
 
-    cell.place_ship(cruiser)
+    cell.place(cruiser)
     cell.fire_upon
 
     assert_equal "H", cell.render
   end
 
   def test_it_renders_x_after_sinking
-    cell = Cell.new("B4")
+    cell = Cell.new
     cruiser = Ship.new("Cruiser", 3)
 
-    cell.place_ship(cruiser)
+    cell.place(cruiser)
     cell.fire_upon
     cell.fire_upon
     cell.fire_upon
@@ -87,20 +81,20 @@ class CellTest < Minitest::Test
   end
 
   def test_it_renders_dot_for_non_fired_cells_with_ship
-    cell = Cell.new("B4")
+    cell = Cell.new
     cruiser = Ship.new("Cruiser", 3)
 
-    cell.place_ship(cruiser)
+    cell.place(cruiser)
 
     assert_instance_of Ship, cell.ship
     assert_equal ".", cell.render
   end
 
   def test_it_renders_s_for_non_fired_cells_with_ship_with_reveal
-    cell = Cell.new("B4")
+    cell = Cell.new
     cruiser = Ship.new("Cruiser", 3)
 
-    cell.place_ship(cruiser)
+    cell.place(cruiser)
 
     assert_instance_of Ship, cell.ship
     assert_equal "S", cell.render(true)
