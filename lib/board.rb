1c1724715ca718414b309f3cab3ef03f6a434846
Aurie <Myrdden42@gmail.com>
2019-03-20 12:02:25 -0600

    Increase board to 10x10

--- lib/board.rb
+++ lib/board.rb
@@ -4,8 +4,8 @@ class Board
     attr_reader :cells
     def initialize
         @cells = {}
-        ('A'..'D').each do |x| #As in x coord of graph
-            ('1'..'4').each do |y|
+        ('A'..'J').each do |x| #As in x coord of graph
+            ('1'..'10').each do |y|
                 @cells[(x+y).to_sym] = Cell.new
             end
         end
@@ -13,4 +13,5 @@ class Board
     def valid_coordinate?(coord)
         @cells.keys.any? {|cell| cell.to_s == coord}
     end
+    def
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -10,9 +10,35 @@ class BoardTest < Minitest::Test
     def test_board_init
         test = Board.new
         expected = {A1: Cell.new, A2: Cell.new, A3: Cell.new, A4: Cell.new,
+                    A5: Cell.new, A6: Cell.new, A7: Cell.new, A8: Cell.new,
+                    A9: Cell.new, A10: Cell.new,
                     B1: Cell.new, B2: Cell.new, B3: Cell.new, B4: Cell.new,
+                    B5: Cell.new, B6: Cell.new, B7: Cell.new, B8: Cell.new,
+                    B9: Cell.new, B10: Cell.new,
                     C1: Cell.new, C2: Cell.new, C3: Cell.new, C4: Cell.new,
-                    D1: Cell.new, D2: Cell.new, D3: Cell.new, D4: Cell.new}
+                    C5: Cell.new, C6: Cell.new, C7: Cell.new, C8: Cell.new,
+                    C9: Cell.new, C10: Cell.new,
+                    D1: Cell.new, D2: Cell.new, D3: Cell.new, D4: Cell.new,
+                    D5: Cell.new, D6: Cell.new, D7: Cell.new, D8: Cell.new,
+                    D9: Cell.new, D10: Cell.new,
+                    E1: Cell.new, E2: Cell.new, E3: Cell.new, E4: Cell.new,
+                    E5: Cell.new, E6: Cell.new, E7: Cell.new, E8: Cell.new,
+                    E9: Cell.new, E10: Cell.new,
+                    F1: Cell.new, F2: Cell.new, F3: Cell.new, F4: Cell.new,
+                    F5: Cell.new, F6: Cell.new, F7: Cell.new, F8: Cell.new,
+                    F9: Cell.new, F10: Cell.new,
+                    G1: Cell.new, G2: Cell.new, G3: Cell.new, G4: Cell.new,
+                    G5: Cell.new, G6: Cell.new, G7: Cell.new, G8: Cell.new,
+                    G9: Cell.new, G10: Cell.new,
+                    H1: Cell.new, H2: Cell.new, H3: Cell.new, H4: Cell.new,
+                    H5: Cell.new, H6: Cell.new, H7: Cell.new, H8: Cell.new,
+                    H9: Cell.new, H10: Cell.new,
+                    I1: Cell.new, I2: Cell.new, I3: Cell.new, I4: Cell.new,
+                    I5: Cell.new, I6: Cell.new, I7: Cell.new, I8: Cell.new,
+                    I9: Cell.new, I10: Cell.new,
+                    J1: Cell.new, J2: Cell.new, J3: Cell.new, J4: Cell.new,
+                    J5: Cell.new, J6: Cell.new, J7: Cell.new, J8: Cell.new,
+                    J9: Cell.new, J10: Cell.new}
 
         assert_equal expected.keys, test.cells.keys
         #assert_equal expected.values, test.cells.values
