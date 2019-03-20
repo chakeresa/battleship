da28cf4a694c364339562ef360df8ad7ad671895
Aurie <Myrdden42@gmail.com>
2019-03-20 13:41:34 -0600

    Add skeleton for player, computer & runner

--- lib/board.rb
+++ lib/board.rb
@@ -20,7 +20,6 @@ class Board
             #Starting coordinate out of bounds?
         return false if coord[0] > (64 + @size).chr || coord[1].to_i > @size
             #Ending coordinate out of bounds?
-            #Mind, letters are
         if horizontal
             #We only need to check the furthest coord from out start.
             return false if coord[1].to_i + ship.length > @size
@@ -39,7 +38,7 @@ class Board
                 return false if !(@cells[at].empty?)
             end
         end
-        #If passed, add ship
+        #If tests passed, add ship
         @ships << ship
         if horizontal
             0.upto(ship.length) do |i|
