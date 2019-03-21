0c796fcad183ce02863ed323cfc3304b3afefd76
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-21 13:14:11 -0600

    Add Computer place method

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,15 +1,31 @@
 require './lib/board'
 
 class Computer
-    def initialize
+  attr_reader :board
 
+  def initialize(size = 10)
+    @board = Board.new(size)
+  end
+
+  def place(ship)
+    valid = :none
+
+    while valid != :success do
+      coord = @board[@board.cells.keys.sample]
+      direction = rand(2)
+      # 0 = right (horizontal), 1 = down (vertical)
+
+      if @board.valid_coordinate?(coord)
+        if direction == 0 # right
+          valid = @board.place(ship, coord, true)
+        else direction == 1 # down
+          valid = @board.place(ship, coord)
+        end
+      end
     end
+  end
 
-    def place(ship)
+  def turn
 
-    end
-
-    def turn
-
-    end
+  end
 end
