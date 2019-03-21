ae0b5641ac4fb074af86817d4b7332fbd510c590
Aurie <Myrdden42@gmail.com>
2019-03-21 13:50:41 -0600

    Remove @ships from Board

--- lib/board.rb
+++ lib/board.rb
@@ -11,7 +11,6 @@ class Board
             end
         end
         @size = size
-        @ships = []
         @render = Render.new
     end
 
@@ -28,7 +27,6 @@ class Board
         return :overlap if overlap?(ship, coord, horizontal)
 
         #If tests passed, add ship
-        @ships << ship
         horizontal ? add_horizontal_ship(ship, coord) : add_vertical_ship(ship, coord)
         return :success
     end
