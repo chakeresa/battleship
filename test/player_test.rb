f3471cd15c4b2f2432922ab0578559bcbe8e3dce
Alexandra Chakeres <alexchakeres@gmail.com>
2019-03-24 20:54:15 -0600

    Add tests of get_dir_input for Player

--- test/player_test.rb
+++ test/player_test.rb
@@ -64,10 +64,172 @@ class PlayerTest < Minitest::Test
     $stdin = STDIN
   end
 
-  # No tests of get_dir_input method because it requires user input
+  def test_player_get_dir_input_quits_w_exclamation
+    player = Player.new("Fred", 2) # board is just A1-B2
+    sub = Ship.new("sub", 2)
+    testInp = StringIO.new
+    testInp.puts "!"
+    testInp.rewind
+    $stdin = testInp
+    result, stdout, stderr = OStreamCatcher.catch do
+      player.get_dir_input("A1", sub)
+    end
+
+    assert_equal :quit, result
+    assert_equal "Pick a direction (left, right, up, down OR l,r,u,d).\n>> ", stdout
+
+    $stdin = STDIN
+  end
+
+  def test_player_get_dir_input_msg_when_invalid_direction
+    player = Player.new("Fred", 2) # board is just A1-B2
+    sub = Ship.new("sub", 2)
+    testInp = StringIO.new
+    testInp.puts "blah"
+    testInp.puts "r"
+    testInp.rewind
+    $stdin = testInp
+    result, stdout, stderr = OStreamCatcher.catch do
+      player.get_dir_input("A1", sub)
+    end
+
+    assert_equal [true, "A1"], result
+    assert_equal "Pick a direction (left, right, up, down OR l,r,u,d).\n>> Invalid direction input.\nPick a direction (left, right, up, down OR l,r,u,d).\n>> ", stdout
+
+    $stdin = STDIN
+  end
+
+  def test_player_get_dir_input_changes_coord_when_left_input
+    player = Player.new("Fred", 2) # board is just A1-B2
+    sub = Ship.new("sub", 2)
+    testInp = StringIO.new
+    testInp.puts "left"
+    testInp.rewind
+    $stdin = testInp
+    result, stdout, stderr = OStreamCatcher.catch do
+      player.get_dir_input("A2", sub)
+    end
+
+    assert_equal [true, "A1"], result
+
+    $stdin = STDIN
+  end
+
+  def test_player_get_dir_input_changes_coord_when_l_input
+    player = Player.new("Fred", 2) # board is just A1-B2
+    sub = Ship.new("sub", 2)
+    testInp = StringIO.new
+    testInp.puts "l"
+    testInp.rewind
+    $stdin = testInp
+    result, stdout, stderr = OStreamCatcher.catch do
+      player.get_dir_input("A2", sub)
+    end
+
+    assert_equal [true, "A1"], result
+
+    $stdin = STDIN
+  end
+
+  def test_player_get_dir_input_changes_coord_when_up_input
+    player = Player.new("Fred", 2) # board is just A1-B2
+    sub = Ship.new("sub", 2)
+    testInp = StringIO.new
+    testInp.puts "up"
+    testInp.rewind
+    $stdin = testInp
+    result, stdout, stderr = OStreamCatcher.catch do
+      player.get_dir_input("B2", sub)
+    end
+
+    assert_equal [false, "A2"], result
+
+    $stdin = STDIN
+  end
+
+  def test_player_get_dir_input_changes_coord_when_u_input
+    player = Player.new("Fred", 2) # board is just A1-B2
+    sub = Ship.new("sub", 2)
+    testInp = StringIO.new
+    testInp.puts "u"
+    testInp.rewind
+    $stdin = testInp
+    result, stdout, stderr = OStreamCatcher.catch do
+      player.get_dir_input("B2", sub)
+    end
+
+    assert_equal [false, "A2"], result
+
+    $stdin = STDIN
+  end
+
+  def test_player_get_dir_input_keeps_coord_when_right_input
+    player = Player.new("Fred", 2) # board is just A1-B2
+    sub = Ship.new("sub", 2)
+    testInp = StringIO.new
+    testInp.puts "right"
+    testInp.rewind
+    $stdin = testInp
+    result, stdout, stderr = OStreamCatcher.catch do
+      player.get_dir_input("B2", sub)
+    end
+
+    assert_equal [true, "B2"], result
+
+    $stdin = STDIN
+  end
+
+  def test_player_get_dir_input_keeps_coord_when_r_input
+    player = Player.new("Fred", 2) # board is just A1-B2
+    sub = Ship.new("sub", 2)
+    testInp = StringIO.new
+    testInp.puts "r"
+    testInp.rewind
+    $stdin = testInp
+    result, stdout, stderr = OStreamCatcher.catch do
+      player.get_dir_input("B2", sub)
+    end
+
+    assert_equal [true, "B2"], result
+
+    $stdin = STDIN
+  end
+
+  def test_player_get_dir_input_keeps_coord_when_down_input
+    player = Player.new("Fred", 2) # board is just A1-B2
+    sub = Ship.new("sub", 2)
+    testInp = StringIO.new
+    testInp.puts "down"
+    testInp.rewind
+    $stdin = testInp
+    result, stdout, stderr = OStreamCatcher.catch do
+      player.get_dir_input("B2", sub)
+    end
+
+    assert_equal [false, "B2"], result
+
+    $stdin = STDIN
+  end
+
+  def test_player_get_dir_input_keeps_coord_when_d_input
+    player = Player.new("Fred", 2) # board is just A1-B2
+    sub = Ship.new("sub", 2)
+    testInp = StringIO.new
+    testInp.puts "d"
+    testInp.rewind
+    $stdin = testInp
+    result, stdout, stderr = OStreamCatcher.catch do
+      player.get_dir_input("B2", sub)
+    end
+
+    assert_equal [false, "B2"], result
+
+    $stdin = STDIN
+  end
+
   # No tests of place method because it requires user input (from get_starting_coord and get_dir_input helper methods)
   # No tests of turn method because it requires user input
-  
+
   def test_player_turn_valid_input
     player1 = Player.new("Fred", 6)
     player2 = Player.new("Mike", 6)
